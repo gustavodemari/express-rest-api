3c37b65933b55dcd5aca8a23ded4744ac2a49341
gustavodemari <guhdemari@gmail.com>
2014-07-17 22:59:52 -0300

    adding express server

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 var config = require('./configs/config.js');
 
-console.log(config);
+var express = require('express');
+
+var app = express();
+
+app.listen(config.server.PORT, function(){
+  console.log('Server running at '+config.server.PORT);
+});
