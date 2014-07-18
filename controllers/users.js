74066607204bb30a062826a2923002fdc88ab060
gustavodemari <guhdemari@gmail.com>
2014-07-17 23:51:45 -0300

    adding new routes and controllers method

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,13 @@
+var users = [{email: 'steve@apple.com', name: 'Steve Jobs'}];
+
 module.exports = {
   list: function(req, res){
-    res.json(200, {users: []});
+    res.json(200, {users: users});
+  },
+  create: function(req, res){
+    res.json(200, {message: 'User created'});
+  },
+  delete: function(req, res){
+    res.json(200, {message: 'Users deleted'});
   }
 }
--- server.js
+++ server.js
@@ -17,7 +17,13 @@ var app = express();
 /* Routes */
 
 router.route('/users')
-  .get(users.list);
+  .get(users.list)
+  .post(users.create);
+
+router.route('/users/:userId')
+  .get(users.list)
+  .put(users.edit);
+  .delete(users.delete);
 
 //Enable CORS
 app.all('*', config.server.CORS_SETUP);
@@ -28,5 +34,5 @@ app.use('/api', router);
 /* Server starting */
 
 app.listen(config.server.PORT, function(){
-  console.log('Server running at '+config.server.PORT);
+  console.log('Server running at '+config.server.PORT);  
 });
