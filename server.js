ef53ad1f95e9f4a5693a8f9982bbde0c6fab4e88
gustavodemari <guhdemari@gmail.com>
2014-07-17 23:09:54 -0300

    adding routes

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
+/* Environment variables */
 var config = require('./configs/config.js');
+
+/* Module loading */
 
 var express = require('express');
 
+/* Module starting */
+
 var app = express();
+var router = express.Router();
+
+/* Routes */
+
+router.route('/users')
+  .get(function(req, res){
+    res.json(200, {users: []});
+  })
+
+//All routes prefixed with /api
+app.use('/api', router)
+
+/* Server starting */
 
 app.listen(config.server.PORT, function(){
   console.log('Server running at '+config.server.PORT);
